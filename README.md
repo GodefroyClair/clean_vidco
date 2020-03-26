6a4901a6878b660b6de8b98ebd5253a338420a32
Godefroy Clair <clair.godefroy@bcgplatinion.com>
2020-03-26 20:46:55 +0100

    working pipeline

--- README.md
+++ README.md
@@ -15,9 +15,10 @@ $ pip install csvkit
 
 Insert the following lines with values in a .env file:
 ```
-export USER=XXXX
-export PWD=XXXX
+export DB_USER=XXXX
+export DB_PWD=XXXX
 export DB=XXX
+export PG_PASSWORD=DB_PWD
 ```
 
 Execute the .env
@@ -33,7 +34,7 @@ $ source txt2sql_covidom.bash
 
 # Create postgres db docker container with a connexion to covidom_csv folder to allow postgres access to the data
 ```
-$ docker run --name covidom -v `pwd`/covidom_csv:/covidom_csv -e POSTGRES_USER=$USER -e POSTGRES_PASSWORD=$PWD -e POSTGRES_DB=$DB -p 5432:5432 -d postgres
+$ docker run --name covidom -v `pwd`/covidom_csv:/covidom_csv -e POSTGRES_USER=$DB_USER -e POSTGRES_PASSWORD=$DB_PWD -e POSTGRES_DB=$DB -p 5432:5432 -d postgres
 # FYI, if necessary /!\, to delete postgres the container: $ docker rm -f covidom
 ```
 
--- txt2sql_covidum.bash
+++ txt2sql_covidum.bash
@@ -7,6 +7,9 @@ zip_filename=$1
 zip_folder=zip_files
 input_folder=input
 csv_folder=covidom_csv
+insert_script=covidom_db_insert_tbls.sql
+create_scrupt=covidom_db_create_tbls.sql
+alter_scrupt=covidom_db_alter_tbls.sql
 
 if [[ ! -f $zip_folder/$zip_filename.zip ]] ; then
     echo "File $zip_filename.zip is not there, aborting."
@@ -14,7 +17,7 @@ if [[ ! -f $zip_folder/$zip_filename.zip ]] ; then
 fi
 
 
-# unzip zip_files/*
+unzip zip_files/*
 mkdir -R $input_folder/$zip_filename
 unzip $zip_folder/$zip_filename.zip -d $input_folder/${zip_filename%.zip}/
 
@@ -23,7 +26,7 @@ echo "unzipped $nb_files files"
 
 mkdir $csv_folder
 
-# convert "|" delimited csv files to comma delimited
+convert "|" delimited csv files to comma delimited
 for filepath in ./$input_folder/$zip_filename/*.txt; do
     filename="$(basename -- $filepath)"
     if [[ ! $filename == *"metadata.txt" ]]
@@ -37,7 +40,17 @@ nb_files=$(ls -1q $csv_folder | wc -l)
 echo "created $nb_files files"
 
 # Ouput a script with create table with no constraints
-# csvsql -d "," -i postgresql $csv_folder/*.csv > covidom_db_create_tbls.sql
+csvsql -d "," -i postgresql $csv_folder/*.csv > covidom_db_create_tbls.sql
+
+# create insert data sql script
+touch $insert_script
+echo "-- this a script to populate data in db\n" > $insert_script
+for csv in ./$csv_folder/*.csv; do
+  csv_filename="$(basename -- $csv)"
+  table=${csv_filename%.csv}
+  echo "COPY $table FROM '/covidom_csv/$table.csv' DELIMITER ',' CSV HEADER;\n" >> $insert_script
+done
+
 
 # rm -rf $input_folder
 # rm -rf $csv_folder
